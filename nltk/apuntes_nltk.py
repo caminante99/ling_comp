e905a80d2bc33fc8c2777510cdfeed0cbf3f9a00
mondeja <mondejar1994@gmail.com>
2017-04-01 00:28:07 +0200

    Update apuntes_nltk.py

--- nltk/apuntes_nltk.py
+++ nltk/apuntes_nltk.py
@@ -2,24 +2,31 @@
 
 import nltk
 
-''' Para instalar: pip install -U nltk '''
+''' Para instalar escribe: pip install -U nltk 
+despu√©s de insertar la ruta de instalaci√≥n de tu Python en el PATH'''
 
-''' Por otro lado, adem·s de la librerÌa tambiÈn es necesario
+''' Si no tienes pip puedes instalarlo ejecutando el archivo
+get_pip.py que hay en esta carpeta. '''
+
+''' Si tienes Windows 32bits puedes usar el archivo.exe
+que hay en esta carpeta para instalar. 
+
+''' Por otro lado, adem√°s de la librer√≠a tambi√©n es necesario
 instalar una serie de ficheros y diccionarios con patrones
-para varios tipos de estructuras gramaticales llamados ìcorporasî,
+para varios tipos de estructuras gramaticales llamados ‚Äúcorporas‚Äù,
 dichos ficheros se instalan de forma independiente por medio de un
 gestor de descargas que puede iniciarse utilizando el modulo nltk '''
 #nltk.download()
 
-''' Invocando a ìdownloadî se abre una ventana en la que se pueden
-gestionar todos los ficheros ìcorporaî en diferentes categorÌas.
-Si es la primera vez que se utiliza nltk, se realizar· la descarga
+''' Invocando a ‚Äúdownload‚Äù se abre una ventana en la que se pueden
+gestionar todos los ficheros ‚Äúcorpora‚Äù en diferentes categor√≠as.
+Si es la primera vez que se utiliza nltk, se realizar√° la descarga
 de dichos ficheros. '''
 
 ''' Los corpus principales que se suelen utilizar en el procesamiento
-de texto son conocidos como ìgutenbergî, el cual incluye una selecciÛn
+de texto son conocidos como ‚Äúgutenberg‚Äù, el cual incluye una selecci√≥n
 de 18 textos del proyecto Gutenberg (http://www.gutenberg.org/)
-y contiene m·s de 1.5 millones de palabras.
+y contiene m√°s de 1.5 millones de palabras.
 Para consultar los textos de gutenberg incluidos en el corpus de NLTK,
 se pueden ejecutar las siguientes instrucciones. '''
 
@@ -29,18 +36,18 @@ from nltk.corpus import gutenberg as gut
 ''' Los textos de la biblioteca hay que insertarlos como unicode '''
 texto = u'shakespeare-hamlet.txt'
 
-''' Contar n˙mero de palabras en un texto '''
+''' Contar n√∫mero de palabras en un texto '''
 num_tokens = len(gut.words(texto))
-''' Token: Se trata de la unidad m·s simple de procesamiento
+''' Token: Se trata de la unidad m√°s simple de procesamiento
 y representa una palabra en el texto. '''
 
-''' Contar n˙mero de caracteres '''
+''' Contar n√∫mero de caracteres '''
 num_caracteres = len(gut.raw(texto))
 
-''' Contar n˙mero de sentencias '''
+''' Contar n√∫mero de sentencias '''
 num_sents = len(gut.sents(texto))
 '''Una sentencia es de punto a punto en lenguas occidentales.
-En otras lenguas es m·s difÌcil determinar quÈ es una sentencia.'''
+En otras lenguas es m√°s dif√≠cil determinar qu√© es una sentencia.'''
 
 
 ''' INSERTAR UN TEXTO PROPIO '''
@@ -64,17 +71,17 @@ implementing them and getting them to work for yourself."""
 sents = sent_tokenize(text)
 tokens = word_tokenize(text)
 
-''' ClasificaciÛn de tokens '''
+''' Clasificaci√≥n de tokens '''
 tagged_tokens = pos_tag(tokens)
 
-''' Consultar informaciÛn sobre los tags '''
+''' Consultar informaci√≥n sobre los tags '''
 #nltk.help.upenn_tagset('JJ')
 
-''' Tokenizar en espaÒol '''
-texto = """El an·lisis autom·tico de sentimiento es un paso m·s
+''' Tokenizar en espa√±ol '''
+texto = """El an√°lisis autom√°tico de sentimiento es un paso m√°s
 en el intento de traducir las emociones humanas a datos.
-Pero la espontaneidad y la inmediatez de la opiniÛn en medios
-sociales hacen que estos sentimientos sean m·s autÈnticos y
+Pero la espontaneidad y la inmediatez de la opini√≥n en medios
+sociales hacen que estos sentimientos sean m√°s aut√©nticos y
 preserven su contenido emocional."""
 
 spanish_tokenizer = nltk.data.load('tokenizers/punkt/spanish.pickle')
@@ -84,23 +91,23 @@ spanish_tokenizer.tokenize(texto)
 ''' API PARA LENGUAJE NATURAL '''
 # http://text-processing.com/demo/
 
-''' An·lisis de texto online '''
+''' An√°lisis de texto online '''
 # http://textanalysisonline.com/nltk-pos-tagging
 
 
 ''' STEEMING '''
-'''En la morfologÌa ling¸Ìstica y la recuperaciÛn de la informaciÛn,
+'''En la morfolog√≠a ling√º√≠stica y la recuperaci√≥n de la informaci√≥n,
 stemming es el proceso para reducir las palabras inflexas
-(o algunas veces derivadas) a su forma de tallo, base o raÌz,
+(o algunas veces derivadas) a su forma de tallo, base o ra√≠z,
 generalmente una forma de palabra escrita.
 
-El tallo no necesita ser idÈntico a la raÌz morfolÛgica de la palabra;
+El tallo no necesita ser id√©ntico a la ra√≠z morfol√≥gica de la palabra;
 normalmente es suficiente que las palabras relacionadas se correspondan
-con el mismo tronco, incluso si este tallo no es en sÌ mismo una raÌz v·lida.
+con el mismo tronco, incluso si este tallo no es en s√≠ mismo una ra√≠z v√°lida.
 
-Los algoritmos para stemming han sido estudiados en inform·tica
-desde la dÈcada de 1960. Muchos motores de b˙squeda tratan palabras
-con el mismo tallo que los sinÛnimos como un tipo de expansiÛn de consulta,
+Los algoritmos para stemming han sido estudiados en inform√°tica
+desde la d√©cada de 1960. Muchos motores de b√∫squeda tratan palabras
+con el mismo tallo que los sin√≥nimos como un tipo de expansi√≥n de consulta,
 un proceso llamado conflation.'''
 
 '''NLTK proporciona varias interfaces de steemers famosos,
@@ -112,45 +119,45 @@ se puede utilizar de esta manera:'''
 
 from nltk.stem.porter import PorterStemmer
 porter_stemmer = PorterStemmer()
-porter_stemmer.stem('maximum')        # uímaximumí
-porter_stemmer.stem('presumably')     # uípresumí
+porter_stemmer.stem('maximum')        # u‚Äômaximum‚Äô
+porter_stemmer.stem('presumably')     # u‚Äôpresum‚Äô
 
 ''' Lancaster Stemmer '''
 from nltk.stem.lancaster import LancasterStemmer
 lancaster_stemmer = LancasterStemmer()
 lancaster_stemmer.stem('maximum')
 
-''' Snowball Stemmer (°Admite el lenguaje espaÒol!) '''
+''' Snowball Stemmer (¬°Admite el lenguaje espa√±ol!) '''
 from nltk.stem import SnowballStemmer
 snowball_stemmer = SnowballStemmer('spanish')
 print(snowball_stemmer.stem('seguridad'))
 
 
 ''' LEMATIZAR '''
-'''El mÈtodo NLTK Lemmatization se basa en la funciÛn morphy
-incorporada de WordNet. WordNetÆ es una gran base de datos lÈxica de inglÈs.
+'''El m√©todo NLTK Lemmatization se basa en la funci√≥n morphy
+incorporada de WordNet. WordNet¬Æ es una gran base de datos l√©xica de ingl√©s.
 
 Los sustantivos, verbos, adjetivos y adverbios se agrupan en conjuntos
-de sinÛnimos sintÈticos (synsets), cada uno de ellos expresando
-un concepto distinto. Las relaciones de sÌntesis est·n interrelacionadas
-mediante relaciones conceptuales-sem·nticas y lÈxicas. La red resultante
+de sin√≥nimos sint√©ticos (synsets), cada uno de ellos expresando
+un concepto distinto. Las relaciones de s√≠ntesis est√°n interrelacionadas
+mediante relaciones conceptuales-sem√°nticas y l√©xicas. La red resultante
 de palabras y conceptos relacionados de forma significativa
 se puede navegar con el navegador.
 
-WordNet tambiÈn est· libre y p˙blicamente disponible para su descarga.
-La estructura de WordNet lo convierte en una herramienta ˙til
-para la ling¸Ìstica computacionaly el procesamiento del lenguaje natural.
+WordNet tambi√©n est√° libre y p√∫blicamente disponible para su descarga.
+La estructura de WordNet lo convierte en una herramienta √∫til
+para la ling√º√≠stica computacionaly el procesamiento del lenguaje natural.
 
 WordNet se asemeja superficialmente a un tesauro, en que agrupa las palabras
 en base a sus significados. Sin embargo, hay algunas distinciones importantes.
-En primer lugar, WordNet enlaza no sÛlo formas de palabras, cadenas de letras,
-sino tambiÈn sentidos especÌficos de palabras.
+En primer lugar, WordNet enlaza no s√≥lo formas de palabras, cadenas de letras,
+sino tambi√©n sentidos espec√≠ficos de palabras.
 
-Como resultado, las palabras que se encuentran en estrecha proximidad entre sÌ
-en la red son sem·nticamente desambiguados. En segundo lugar, WordNet
-etiqueta las relaciones sem·nticas entre palabras, mientras que
-los agrupamientos de palabras en un tesauro no siguen ning˙n patrÛn
-explÌcito que no sea la similitud de significado. '''
+Como resultado, las palabras que se encuentran en estrecha proximidad entre s√≠
+en la red son sem√°nticamente desambiguados. En segundo lugar, WordNet
+etiqueta las relaciones sem√°nticas entre palabras, mientras que
+los agrupamientos de palabras en un tesauro no siguen ning√∫n patr√≥n
+expl√≠cito que no sea la similitud de significado. '''
 
 from nltk.stem import WordNetLemmatizer
 wordnet_lemmatizer = WordNetLemmatizer()
@@ -158,13 +165,13 @@ wordnet_lemmatizer.lemmatize('dogs')    # u'dog'
 wordnet_lemmatizer.lemmatize('are')     # u'are'
 wordnet_lemmatizer.lemmatize('is')      # u'is'
 
-'''NotarÌa que los resultados de "are" y "is" no son "be",
+'''Notar√≠a que los resultados de "are" y "is" no son "be",
 es porque el argumento lemmatize por defecto es "n" (name): '''
 
-#lemmatize(word, pos=íní)
+#lemmatize(word, pos=‚Äôn‚Äô)
 
-''' AsÌ que hay que espicificarle que busque el verbo '''
-print(wordnet_lemmatizer.lemmatize('is', pos='v'))     # uíbeí
+''' As√≠ que hay que espicificarle que busque el verbo '''
+print(wordnet_lemmatizer.lemmatize('is', pos='v'))     # u‚Äôbe‚Äô
 
 
 
